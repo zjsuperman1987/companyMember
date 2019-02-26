559db8bf4f36eb77d4c70d0c160483e61efd04a5
zyEros <395693101@qq.com>
2019-02-27 00:52:53 +0800

    home

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -1,16 +1,23 @@
 'use strict'
 const path = require('path')
-const baseWebpackConfig = require('./webpack.base.conf.js')
+// const baseWebpackConfig = require('./webpack.base.conf.js')
 const config = require('../config')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
 	context: path.resolve(__dirname, '../'),
 	entry: {
-		main: './src/index/index.js',
+		index: './src/index/index.js',
 		test: './src/test/test.js'
 	},
 	output: {
 		path: config.build.assetsRoot,
 		filename: '[name]/[name].js',
-	}
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			filename: 'index/index.html',
+			template: "./src/index/index.html"
+		})
+	]
 }
